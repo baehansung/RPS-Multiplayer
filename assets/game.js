dc5376c1347b2d5fd0a4aeb6c514842bb2b4b940
Han Sung Bae <hansungbae@Hans-MacBook-Air.local>
2018-06-28 18:44:06 -0400

    adjusted HTML and CSS

--- assets/game.js
+++ assets/game.js
@@ -1,18 +1,56 @@
 var score1 = 0;
 var score2 = 0;
+var user1 = "";
+var user2 = "";
 
 $(document).ready(function(){
 
-    $("#rockbtn").on("click", function(){
-        console.log("clicked rock");
+    RPS = ["Rock", "Paper", "Scissors"];
+
+    rockbtn = $("#rockbtn").on("click", function(){
+        user1 = RPS[0];
     });
 
-    $("#paperbtn").on("click", function(){
+    paperbtn = $("#paperbtn").on("click", function(){
         console.log("clicked paper");
     });
 
-    $("#scissorsbtn").on("click", function(){
+    scissorsbtn = $("#scissorsbtn").on("click", function(){
         console.log("clicked scissors");
     });
+
+    // function play() {
+    
+    //     if(user1 === RPS[0] && user2 === RPS[1]) {
+    //       console.log("user2, you're the winner, paper beats rock!");
+    //       winUser2 ++;
+    //     } else if(user1 === RPS[0] && user2 === RPS[2]) {
+    //       console.log("user1, you're the winner, rock beats scissors!");
+    //       winUser1 ++;
+    //     } else if(user1 === RPS[0] && user2 === RPS[0]) {
+    //       console.log("You both tie since you both chose rock!");
+    //       draw ++;
+    //     } else if(user1 === RPS[1] && user2 === RPS[0]) {
+    //       console.log("user 1, you're the winner, paper beats rock!");
+    //       winUser1 ++;
+    //     } else if(user1 === RPS[1] && user2 === RPS[1]) {
+    //       console.log("You both tie since you both chose paper!");
+    //       draw ++;
+    //     } else if(user1 === RPS[1] && user2 === RPS[2]) {
+    //       console.log("user 2, you're the winner, scissors beats paper!");
+    //       winUser2 ++;
+    //     } else if(user1 === RPS[2] && user2 === RPS[0]) {
+    //       console.log("user2, you're the winner, rock beats scissors!");
+    //       winUser2 ++;
+    //     } else if(user1 === RPS[2] && user2 === RPS[1]) {
+    //       console.log("user1, you're the winner, scissors beats paper!");
+    //       winUser1 ++;
+    //     } else {
+    //       console.log("You both tie since you both chose scissors!");
+    //       draw ++;
+    //     }
+    // };
+
+    // play();
 
 });
